83f76594489b2621afeb97e2680be67d0741bf41
Advay Patil <advaypatil27@gmail.com>
2024-10-29 11:40:12 -0700

    random ahh change

--- index.ts
+++ index.ts
@@ -3,24 +3,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // Fetch all Users
-  // Expected output: []
-  const allUsers = await prisma.user.findMany();
-  console.log(allUsers);
-
-  await prisma.user.create({
-    data: {
-      email: "apat23@uw.edu",
-      name: "Advay",
-      posts: {
-        create: { title: "Hello, UW" },
-      },
-      profile: {
-        create: { bio: "I like UW" },
-      },
-    },
-  });
-
   const updatedUsers = await prisma.user.findMany({
     include: {
       posts: true,
