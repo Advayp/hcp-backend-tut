04852ab3a6e18514f32f9766b5019aafda63baac
Advay Patil <advaypatil27@gmail.com>
2024-10-29 19:05:25 -0700

    add fetching of users

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,38 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+type PostRequest = {
+  email: string;
+  name: string;
+};
+
+type PostResponse = {
+  success: boolean;
+};
+
+export async function POST(
+  req: NextRequest
+): Promise<NextResponse<PostResponse>> {
+  const { email, name }: PostRequest = await req.json();
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      profile: {
+        create: {
+          bio: "bio",
+        },
+      },
+      posts: {
+        create: {
+          title: "Title 1",
+        },
+      },
+    },
+  });
+
+  console.log(user);
+
+  return NextResponse.json({ success: true });
+}
